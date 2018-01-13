67490ec0cca027c0408e48ed15a9a9a1dbacdcc3
Zehaos <shizehao.scu@gmail.com>
2018-01-13 19:09:12 +0800

    act after bn

--- symbol/pointnet_cls.py
+++ symbol/pointnet_cls.py
@@ -21,13 +21,13 @@ def pointnet(num_classes, batch_size, num_points, bn_mom=0.9, workspace=512, sco
     # Shared mlp
     conv0 = mx.sym.Convolution(data=input_image, num_filter=64, kernel=(1, 3), stride=(1, 1), name=scope + "conv0",
                                workspace=workspace)
-    conv0 = mx.sym.Activation(data=conv0, act_type='relu', name=scope + 'relu0')
     conv0 = mx.sym.BatchNorm(data=conv0, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn0')
+    conv0 = mx.sym.Activation(data=conv0, act_type='relu', name=scope + 'relu0')
 
     conv1 = mx.sym.Convolution(data=conv0, num_filter=64, kernel=(1, 1), stride=(1, 1), name=scope + "conv1",
                                workspace=workspace)
-    conv1 = mx.sym.Activation(data=conv1, act_type='relu', name=scope + 'relu1')
     conv1 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn1') # (B, 64, 1024, 1)
+    conv1 = mx.sym.Activation(data=conv1, act_type='relu', name=scope + 'relu1')
 
     # Feature transformer
     transform = feature_transform_net(conv1, batch_size, num_points, workspace, bn_mom, scope=scope + "ftn_") # (B, 64, 64)
@@ -40,30 +40,30 @@ def pointnet(num_classes, batch_size, num_points, bn_mom=0.9, workspace=512, sco
     conv2 = mx.sym.Convolution(data=conv1_transformed, num_filter=64, kernel=(1, 1), stride=(1, 1),
                                name=scope + "conv2",
                                workspace=workspace)
-    conv2 = mx.sym.Activation(data=conv2, act_type='relu', name=scope + 'relu2')
     conv2 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn2')
+    conv2 = mx.sym.Activation(data=conv2, act_type='relu', name=scope + 'relu2')
 
     conv3 = mx.sym.Convolution(data=conv2, num_filter=128, kernel=(1, 1), stride=(1, 1), name=scope + "conv3",
                                workspace=workspace)
-    conv3 = mx.sym.Activation(data=conv3, act_type='relu', name=scope + 'relu3')
     conv3 = mx.sym.BatchNorm(data=conv3, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn3')
+    conv3 = mx.sym.Activation(data=conv3, act_type='relu', name=scope + 'relu3')
 
     conv4 = mx.sym.Convolution(data=conv3, num_filter=1024, kernel=(1, 1), stride=(1, 1), name=scope + "conv4",
                                workspace=workspace)
-    conv4 = mx.sym.Activation(data=conv4, act_type='relu', name=scope + 'relu4')
     conv4 = mx.sym.BatchNorm(data=conv4, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn4')
+    conv4 = mx.sym.Activation(data=conv4, act_type='relu', name=scope + 'relu4')
 
     pool5 = mx.sym.Pooling(data=conv4, kernel=(num_points, 1), pool_type='max', name=scope + 'pool5')
     pool5_reshaped = mx.sym.Reshape(data=pool5, shape=(batch_size, -1), name=scope + 'pool5_reshape')
 
     fc6 = mx.sym.FullyConnected(data=pool5_reshaped, num_hidden=512, name=scope + 'fc6')
-    fc6 = mx.sym.Activation(data=fc6, act_type='relu', name=scope + 'relu6')
     fc6 = mx.sym.BatchNorm(data=fc6, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn6')
+    fc6 = mx.sym.Activation(data=fc6, act_type='relu', name=scope + 'relu6')
     fc6 = mx.sym.Dropout(fc6, p=0.7)
 
     fc7 = mx.sym.FullyConnected(data=fc6, num_hidden=256, name=scope + 'fc7')
-    fc7 = mx.sym.Activation(data=fc7, act_type='relu', name=scope + 'relu7')
     fc7 = mx.sym.BatchNorm(data=fc7, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn7')
+    fc7 = mx.sym.Activation(data=fc7, act_type='relu', name=scope + 'relu7')
     fc7 = mx.sym.Dropout(fc7, p=0.7)
 
     fc8 = mx.sym.FullyConnected(data=fc7, num_hidden=40, name=scope + 'fc8')
--- symbol/transform_nets.py
+++ symbol/transform_nets.py
@@ -9,29 +9,29 @@ def input_transform_net(data, batch_size, num_points, workspace, bn_mom=0.9, sco
     data = mx.sym.expand_dims(data, axis=1)  # (32,1,1024,3)
     conv0 = mx.sym.Convolution(data=data, num_filter=64, kernel=(1, 3), stride=(1, 1), name=scope + "conv0",
                                workspace=workspace)
-    conv0 = mx.sym.Activation(data=conv0, act_type='relu', name=scope + 'relu0')
     conv0 = mx.sym.BatchNorm(data=conv0, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn0')
+    conv0 = mx.sym.Activation(data=conv0, act_type='relu', name=scope + 'relu0')
 
     conv1 = mx.sym.Convolution(data=conv0, num_filter=128, kernel=(1, 1), stride=(1, 1), name=scope + "conv1",
                                workspace=workspace)
-    conv1 = mx.sym.Activation(data=conv1, act_type='relu', name=scope + 'relu1')
     conv1 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn1')
+    conv1 = mx.sym.Activation(data=conv1, act_type='relu', name=scope + 'relu1')
 
     conv2 = mx.sym.Convolution(data=conv1, num_filter=1024, kernel=(1, 1), stride=(1, 1), name=scope + "conv2",
                                workspace=workspace)
-    conv2 = mx.sym.Activation(data=conv2, act_type='relu', name=scope + 'relu2')
     conv2 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn2')
+    conv2 = mx.sym.Activation(data=conv2, act_type='relu', name=scope + 'relu2')
 
     pool3 = mx.sym.Pooling(data=conv2, kernel=(num_points, 1), pool_type='max', name=scope + 'pool3')
     pool3_reshaped = mx.sym.Reshape(data=pool3, shape=(batch_size, -1))
 
     fc4 = mx.sym.FullyConnected(data=pool3_reshaped, num_hidden=512, name=scope + 'fc4')
-    fc4 = mx.sym.Activation(data=fc4, act_type='relu', name=scope + 'relu4')
     fc4 = mx.sym.BatchNorm(data=fc4, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn4')
+    fc4 = mx.sym.Activation(data=fc4, act_type='relu', name=scope + 'relu4')
 
     fc5 = mx.sym.FullyConnected(data=fc4, num_hidden=256, name=scope + 'fc5')
-    fc5 = mx.sym.Activation(data=fc5, act_type='relu', name=scope + 'relu5')
     fc5 = mx.sym.BatchNorm(data=fc5, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn5')
+    fc5 = mx.sym.Activation(data=fc5, act_type='relu', name=scope + 'relu5')
 
     input_transformer_weight = mx.sym.Variable(name="input_transformer_weight", shape=(9, 256), init=mx.init.Zero())
     input_transformer_bias = mx.sym.Variable(name="input_transformer_bias", shape=(9), init=mx.init.Zero())
@@ -50,29 +50,29 @@ def input_transform_net(data, batch_size, num_points, workspace, bn_mom=0.9, sco
 def feature_transform_net(data, batch_size, num_points, workspace, bn_mom=0.9, scope="ftn_"):
     conv0 = mx.sym.Convolution(data=data, num_filter=64, kernel=(1, 1), stride=(1, 1), name=scope + "conv0",
                                workspace=workspace)
-    conv0 = mx.sym.Activation(data=conv0, act_type='relu', name=scope + 'relu0')
     conv0 = mx.sym.BatchNorm(data=conv0, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn0')
+    conv0 = mx.sym.Activation(data=conv0, act_type='relu', name=scope + 'relu0')
 
     conv1 = mx.sym.Convolution(data=conv0, num_filter=128, kernel=(1, 1), stride=(1, 1), name=scope + "conv1",
                                workspace=workspace)
-    conv1 = mx.sym.Activation(data=conv1, act_type='relu', name=scope + 'relu1')
     conv1 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn1')
+    conv1 = mx.sym.Activation(data=conv1, act_type='relu', name=scope + 'relu1')
 
     conv2 = mx.sym.Convolution(data=conv1, num_filter=1024, kernel=(1, 1), stride=(1, 1), name=scope + "conv2",
                                workspace=workspace)
-    conv2 = mx.sym.Activation(data=conv2, act_type='relu', name=scope + 'relu2')
     conv2 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn2')
+    conv2 = mx.sym.Activation(data=conv2, act_type='relu', name=scope + 'relu2')
 
     pool3 = mx.sym.Pooling(data=conv2, kernel=(num_points, 1), pool_type='max', name=scope + 'pool3')
     pool3_reshaped = mx.sym.Reshape(data=pool3, shape=(batch_size, -1))
 
     fc4 = mx.sym.FullyConnected(data=pool3_reshaped, num_hidden=512, name=scope + 'fc4')
-    fc4 = mx.sym.Activation(data=fc4, act_type='relu', name=scope + 'relu4')
     fc4 = mx.sym.BatchNorm(data=fc4, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn4')
+    fc4 = mx.sym.Activation(data=fc4, act_type='relu', name=scope + 'relu4')
 
     fc5 = mx.sym.FullyConnected(data=fc4, num_hidden=256, name=scope + 'fc5')
-    fc5 = mx.sym.Activation(data=fc5, act_type='relu', name=scope + 'relu5')
     fc5 = mx.sym.BatchNorm(data=fc5, fix_gamma=False, eps=eps, momentum=bn_mom, name=scope + 'bn5')
+    fc5 = mx.sym.Activation(data=fc5, act_type='relu', name=scope + 'relu5')
 
     feat_transformer_weight = mx.sym.Variable(name="feat_transformer_weight", shape=(64*64, 256), init=mx.init.Zero())
     feat_transformer_bias = mx.sym.Variable(name="feat_transformer_bias", shape=(64*64), init=mx.init.Zero())
